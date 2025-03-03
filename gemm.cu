3fa3b9856e98349827617742b9b296ce2493af86
Bert Maher <bertrand@fb.com>
2025-03-03 11:39:44 -0800

    implemented TMA loads, maybe even correctly

--- gemm.cu
+++ gemm.cu
@@ -2,6 +2,7 @@
 #include <cuda_bf16.h>
 #include <cublas_v2.h>
 #include <stdio.h>
+#include <assert.h>
 
 using bf16 = __nv_bfloat16;
 
@@ -16,6 +17,104 @@ void checkCudaErrors(cudaError_t error, const char* file, int line) {
 
 int cdiv(int m, int n) {
   return (m + n - 1) / n;
+}
+
+__host__ static inline CUtensorMap create_tma_desc(bf16* gmem, int M, int N, int BLOCK_M, int BLOCK_N)  {
+  CUtensorMap tma_desc;
+  // TODO: Check these requirements against the HW spec.
+  assert(BLOCK_N >= 64);
+  assert(N % 64 == 0);
+
+  // TODO: cdiv?
+  // TODO" why the 64 inner dim?
+  uint64_t shape[] = {64, M, N / 64};
+  uint64_t stride[] = {sizeof(bf16) * N, 64 * sizeof(bf16)};
+  uint32_t box_shape[] = {64, BLOCK_M, BLOCK_N / 64};
+  uint32_t box_stride[] = {1, 1, 1};
+
+  auto result = cuTensorMapEncodeTiled(
+            &tma_desc,
+            CU_TENSOR_MAP_DATA_TYPE_BFLOAT16,
+            3,
+            gmem,
+            shape,
+            stride,
+            box_shape,
+            box_stride,
+            CU_TENSOR_MAP_INTERLEAVE_NONE,
+            CU_TENSOR_MAP_SWIZZLE_128B,
+            CU_TENSOR_MAP_L2_PROMOTION_NONE,
+            CU_TENSOR_MAP_FLOAT_OOB_FILL_NONE
+  );
+
+  if (result != CUDA_SUCCESS) {
+    fprintf(stderr, "TMA desc creation failed\n");
+    exit(EXIT_FAILURE);
+  }
+
+  return tma_desc;
+}
+
+template <uint32_t REGS>
+__device__ void setmaxnreg_inc() {
+  asm volatile("setmaxnreg.inc.sync.aligned.u32 %0;\n" : : "n"(REGS));
+}
+
+template <uint32_t REGS>
+__device__ void setmaxnreg_dec() {
+  asm volatile("setmaxnreg.dec.sync.aligned.u32 %0;\n" : : "n"(REGS));
+}
+
+__device__ void init_barrier(uint64_t* bar, int thread_count, int transaction_count) {
+  uint32_t bar_ptr = static_cast<uint32_t>(__cvta_generic_to_shared(bar));
+  asm volatile(
+      "mbarrier.init.shared::cta.b64 [%0], %1;\n"
+      :: "r"(bar_ptr), "r"(thread_count + transaction_count));
+}
+
+__device__ void wait_barrier(uint64_t* bar, int phase) {
+  uint32_t mbar_ptr = static_cast<uint32_t>(__cvta_generic_to_shared(bar));
+  asm volatile(
+      "{\n"
+      ".reg .pred P1;\n"
+      "LAB_WAIT:\n"
+      "mbarrier.try_wait.parity.shared::cta.b64 P1, [%0], %1;\n"
+      "@P1 bra.uni DONE;\n"
+      "bra.uni LAB_WAIT;\n"
+      "DONE:\n"
+      "}\n"
+      :: "r"(mbar_ptr), "r"(phase)
+  );
+}
+
+__device__ void arrive_barrier(uint64_t* bar, int count) {
+  uint32_t bar_ptr = static_cast<uint32_t>(__cvta_generic_to_shared(bar));
+  asm volatile(
+      "mbarrier.arrive.release.cta.shared::cta.b64 _, [%0], %1;\n"
+      :: "r"(bar_ptr), "r"(count) : "memory");
+}
+
+__device__ void expect_bytes(uint64_t* bar, uint32_t bytes) {
+  uint32_t bar_ptr = static_cast<uint32_t>(__cvta_generic_to_shared(bar));
+  asm volatile("mbarrier.arrive.expect_tx.shared::cta.b64 _, [%0], %1;\n"
+               :: "r"(bar_ptr), "r"(bytes));
+}
+
+__device__ void tma_load(bf16* dst, void const* const src_tma_desc, uint64_t* bar, int n, int m) {
+  uint64_t tma_ptr = reinterpret_cast<uint64_t>(src_tma_desc);
+  uint32_t bar_ptr = static_cast<uint32_t>(__cvta_generic_to_shared(bar));
+  uint32_t dst_ptr = static_cast<uint32_t>(__cvta_generic_to_shared(dst));
+  asm volatile(
+      "cp.async.bulk.tensor.3d.shared::cluster.global.tile.mbarrier::complete_tx::bytes"
+      " [%0], [%1, {%3, %4, %5}], [%2];"
+      ::
+       "r"(dst_ptr),
+       "l"(tma_ptr),
+       "r"(bar_ptr),
+       "n"(0),
+       "r"(m),
+       "r"(n / 64)
+      : "memory");
 }
 
 __global__ void testFill(bf16* X, int M, int N, int parity) {
@@ -49,7 +148,7 @@ void runCublasGemmBF16(int M, int N, int K, bf16 *A, bf16 *B, bf16 *C) {
   }
 }
 
-__global__ void gemm(bf16* A, bf16* B, bf16* C, int M, int N, int K) {
+__global__ void naive_gemm(bf16* A, bf16* B, bf16* C, int M, int N, int K) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < M * N) {
     int m_idx = idx % M;
@@ -62,13 +161,129 @@ __global__ void gemm(bf16* A, bf16* B, bf16* C, int M, int N, int K) {
   }
 }
 
-int main() {
-  int m = 6 * 11 * 128;
-  int n = 6 * 12 * 128;
-  int k = 256;
+void run_naive_gemm(bf16* A, bf16* B, bf16* C, int M, int N, int K) {
+  naive_gemm<<<cdiv(M * N, 1024), 1024>>>(A, B, C, M, N, K);
+}
 
-  m = k = 8;
-  n = 16;
+constexpr int BLOCK_M = 128;
+constexpr int BLOCK_N = 256;
+constexpr int BLOCK_K = 64;
+constexpr int NUM_SMS = 132;
+constexpr int STAGES = 3;
+constexpr int WARPGROUP_SIZE = 128;
+constexpr int WARPGROUPS = 3;
+constexpr int NUM_THREADS = WARPGROUPS * WARPGROUP_SIZE;
+
+struct SharedStorage {
+  alignas(128) bf16 A[BLOCK_M * BLOCK_K * STAGES];
+  alignas(128) bf16 B[BLOCK_K * BLOCK_N * STAGES];
+};
+
+__global__ __launch_bounds__(NUM_THREADS) void gemm(const __grid_constant__ CUtensorMap A, const __grid_constant__ CUtensorMap B, bf16* C, int M, int N, int K) {
+  // Producer buffers for A and B.
+  extern __shared__ __align__(128) uint8_t dynamic_smem[];
+  SharedStorage& smem = *reinterpret_cast<SharedStorage*>(dynamic_smem);
+
+  // Barriers.
+  __shared__ __align__(8) uint64_t prod[STAGES];
+  __shared__ __align__(8) uint64_t cons[STAGES];
+
+  int tid = threadIdx.x;
+  int wgid = tid / WARPGROUP_SIZE;
+  int wg_tid = tid % WARPGROUP_SIZE;
+
+  // Init barriers.
+  if (tid == 0) {
+    for (int i = 0; i < STAGES; i++) {
+      init_barrier(&prod[i], 0, 1);
+      init_barrier(&cons[i], 0, WARPGROUPS - 1);
+    }
+  }
+  __syncthreads();
+
+  if (wgid == 0) {
+    // Producer warpgroup.
+    setmaxnreg_dec<40>();
+    int phase = 0;
+    // Mainloop.
+    int m = 0, n = 0;
+    if (wg_tid == 0) {
+      for (int k = 0; k < K; k += BLOCK_K) {
+        // Wait for consumer.
+        // TODO: stage and phase update.
+        wait_barrier(&cons[0], phase);
+        // Set expect bytes for TMA.
+        expect_bytes(&prod[0], sizeof(bf16) * (BLOCK_M * BLOCK_K + BLOCK_K * BLOCK_N));
+        // Load A.
+        // TODO: use proper stage
+        tma_load(&smem.A[0], &A, &prod[0], k * BLOCK_K, m * BLOCK_M);
+        // Load B.
+        tma_load(&smem.B[0], &B, &prod[0], k * BLOCK_K, n * BLOCK_N);
+      }
+    }
+  } else {
+    // Consumer warpgroup.
+    setmaxnreg_inc<240>();
+    int phase = 0;
+    if (wg_tid == 0) {
+      arrive_barrier(&cons[0], 1);
+    }
+    // Mainloop.
+    for (int k = 0; k < K; k += BLOCK_K) {
+      // Wait for producer.
+      wait_barrier(&prod[0], phase);
+      // Perform wgmma.
+      // Arrive at consumer.
+    }
+    // Write back to gmem.
+  }
+  // __syncthreads();
+  // if (tid == 128) {
+  //   printf("smem.A:\n");
+  //   for (int i = 0; i < BLOCK_M; i++) {
+  //     for (int j = 0; j < BLOCK_K; j++) {
+  //       printf("  %6.2f", __bfloat162float(smem.A[i * BLOCK_K + j]));
+  //     }
+  //     printf("\n");
+  //   }
+  //   printf("\n");
+  //   printf("smem.B:\n");
+  //   for (int i = 0; i < BLOCK_K; i++) {
+  //     for (int j = 0; j < BLOCK_N; j++) {
+  //       printf("  %6.2f", __bfloat162float(smem.B[i + j * BLOCK_K]));
+  //     }
+  //     printf("\n");
+  //   }
+  //   printf("\n");
+  // }
+}
+
+void run_gemm(bf16* A, bf16* B, bf16* C, int M, int N, int K) {
+  // Compute necessary shared memory for buffers.
+  size_t smem_size = sizeof(SharedStorage);
+  check(cudaFuncSetAttribute(gemm, cudaFuncAttributeMaxDynamicSharedMemorySize, smem_size));
+
+  // Set up TMA descriptors
+  auto descA = create_tma_desc(A, M, K, BLOCK_M, BLOCK_K);
+  auto descB = create_tma_desc(B, N, K, BLOCK_N, BLOCK_K);
+
+  // Launch kernel!
+  gemm<<<1, NUM_THREADS, smem_size>>>(descA, descB, C, M, N, K);
+  check(cudaDeviceSynchronize());
+  check(cudaGetLastError());
+}
+
+int main() {
+  // int m = 6 * 11 * 128;
+  // int n = 6 * 12 * 128;
+  // int k = 512;
+
+  //m = k = 8;
+  //n = 16;
+
+  int m = 128;
+  int n = 256;
+  int k = 64;
 
   int max = 16384;
 
@@ -94,7 +309,7 @@ int main() {
   runCublasGemmBF16(m, n, k, A, B, Cref);
 
   // Run test kernel.
-  gemm<<<cdiv(m * n, 1024), 1024>>>(A, B, C, m, n, k);
+  run_gemm(A, B, C, m, n, k);
 
   // Print a slab of matrix for sanity.
   bf16* hostA = (bf16*)malloc(sizeof(bf16) * numel);
@@ -137,19 +352,19 @@ int main() {
   // Test against cuBLAS reference.
   bf16* hostC = nullptr;
   bf16* hostCref = nullptr;
-  if (false) {
+  if (true) {
     hostC = (bf16*)malloc(sizeof(bf16) * m * n);
     hostCref = (bf16*)malloc(sizeof(bf16) * m * n);
 
     check(cudaMemcpy(hostC, C, sizeof(bf16) * m * n, cudaMemcpyDeviceToHost));
     check(cudaMemcpy(hostCref, Cref, sizeof(bf16) * m * n, cudaMemcpyDeviceToHost));
 
-    exit(0);
     for (int i = 0; i < m * n; i++) {
       float cv = __bfloat162float(hostC[i]);
       float crefv = __bfloat162float(hostCref[i]);
       if (std::abs(cv - crefv) > 1e-5) {
         fprintf(stderr, "Failed tolerance check at idx %d, C=%f, Cref=%f\n", i, cv, crefv);
+        exit(EXIT_FAILURE);
       }
     }
   }
